e2ed4748ad7ededad4f52e1b3247ed2391496bbe
Yago Milano <yago.goncalves@invillia.com>
2022-02-03 05:18:54 -0300

    refatorando o modo de renderização da página de SSR para SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -52,7 +52,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KOyn8LfLcb1fuM2QQqKqVNi')
 
   const product = {
@@ -66,6 +66,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
   }
 }
