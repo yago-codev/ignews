164e6564f692b94529a8ebbba258b4066e42b18a
Yago Milano <yago.goncalves@invillia.com>
2022-02-03 00:27:42 -0300

    cria o componente Header & SignInButton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import Head from 'next/head'
 
 import { AppProps } from 'next/app'
 
+import { Header } from '../components/Header';
+
+import '../styles/global.scss';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>ig.news</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
     </>
   )
