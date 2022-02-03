b5d5af3943174d12a9a47e0a0183d76706f45621
Yago Milano <yago.goncalves@invillia.com>
2022-02-03 01:01:11 -0300

    cria a página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,39 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+            👏
+          <span>
+            Hey, welcome
+          </span>
+          <h1>
+            <span>
+              News about
+            </span>
+            <div>
+              <span>the</span> <span>React</span> {' '}
+              world.
+            </div>
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img
+          src="/images/avatar.svg"
+          alt="Ilustração de uma garota programando em seu notebook"
+        />
+      </main>
     </>
   )
 }
