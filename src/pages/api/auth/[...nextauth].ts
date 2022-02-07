7f9b18dcc6ef4462046da04d4e33f6a0bea98524
Yago Milano <yago.goncalves@invillia.com>
2022-02-06 21:26:10 -0300

    persistindo usuário no FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb"
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,24 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn(user) {
+      const { user: userGithub } = user
+      const { email } = userGithub
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
 })
