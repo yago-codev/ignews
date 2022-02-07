4921ff13a84f854e2d290188781e048d06b2b63f
Yago Milano <yago.goncalves@invillia.com>
2022-02-07 01:26:48 -0300

    criando checkout session no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { signIn, useSession } from 'next-auth/react'
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +7,20 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { status } = useSession()
+  
+  function handleSubscribe() {
+    if (status === 'unauthenticated') {
+      signIn('github')
+      return
+    }
+  }
+  
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
